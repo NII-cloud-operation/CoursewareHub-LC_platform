bf60f00dc214603d6bcb2b6dbf7e0171ed83a517
Richard Potter <potter@axsh.net>
2016-10-03 21:58:18 +0900

    Changed port forwarding to target 192.168.11.* addresses

--- demo-scripts/google-oauth-distribute-notebooks-via-dir-softlink.sh
+++ demo-scripts/google-oauth-distribute-notebooks-via-dir-softlink.sh
@@ -18,20 +18,20 @@ randomport="$(( 5000 + ( $RANDOM % 5000 ) ))"
 
 mkdir-conf-file()
 {
-    [ -f "$3" ] && return
-    cat >"$3" <<EOF
+    [ -f "$4" ] && return
+    cat >"$4" <<EOF
 # Not all of these are used on every VM
 export EXTRAHOSTFWDREL=""
 
 # port 22 is already assigned by kvmsteps
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::43-:43
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::80-:80
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::81-:8001
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::83-:8000
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::84-:8888
-EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::90-:9000
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::43-${2}:43
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::80-${2}:80
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::81-${2}:8001
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::83-${2}:8000
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::84-${2}:8888
+EXTRAHOSTFWDREL=\$EXTRAHOSTFWDREL,hostfwd=tcp::90-${2}:9000
 
-export mcastMAC="$2"
+export mcastMAC="$3"
 export mcastPORT=$randomport
 EOF
     
@@ -39,16 +39,16 @@ EOF
 
 # last 2 digits, MACaddr, filename
 cinfo="
-10 52:54:00:12:00:99 datadir-jh.conf
-20 52:54:00:12:00:09 datadir-jh-hub.conf
-30 52:54:00:12:00:01 datadir-jh-node1.conf
-40 52:54:00:12:00:02 datadir-jh-node2.conf
-50 52:54:00:12:00:90 datadir-1box.conf
+10 192.168.11.99 52:54:00:12:00:99 datadir-jh.conf
+20 192.168.11.88 52:54:00:12:00:09 datadir-jh-hub.conf
+30 192.168.11.1  52:54:00:12:00:01 datadir-jh-node1.conf
+40 192.168.11.2  52:54:00:12:00:02 datadir-jh-node2.conf
+50 192.168.11.90 52:54:00:12:00:90 datadir-1box.conf
 "
 
-while read twodigits mac fname ; do
+while read twodigits ipaddress mac fname ; do
     [ "$fname" == "" ] && continue
-    mkdir-conf-file "$twodigits" "$mac" "$fname"
+    mkdir-conf-file "$twodigits" "$ipaddress" "$mac" "$fname"
 done <<<"$cinfo"
 
 # make sure mcast addresses are the same
