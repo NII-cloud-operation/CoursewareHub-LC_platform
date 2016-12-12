d3536dc40ba4543efb703cdbeedcaf761aaf69dd
Richard Potter <potter@axsh.net>
2016-12-12 21:18:52 +0900

    Hub kvm now has extra nics, soon to connect directly to nodes. Everything still works.

--- setup-networking-alternative-1.sh
+++ setup-networking-alternative-1.sh
@@ -21,7 +21,7 @@ runonvm()
     if [ -f "$avmpath/proxy-shell.sh" ]; then
 	"$avmpath/proxy-shell.sh"
     else
-	( cd "$avmpath" &&  bash	) || exit
+	( cd "$avmpath" &&  bash	) || return 1
     fi
 }
 
@@ -56,4 +56,46 @@ runonvm()
 	do1_morecpu "$i" ; prev_cmd_failed
 	do1_moremem "$i" ; prev_cmd_failed
     done
+) ; prev_cmd_failed
+
+(
+    $starting_group "Setup hub for networking using qemu socket"
+    false
+    $skip_group_if_unnecessary
+
+    (
+	avmdir=jhvmdir-hub
+	$starting_step "Patch hubs kvm-boot.sh"
+	output="$(runonvm "$DATADIR/$avmdir" <<<'grep -F -e "mcastnet" kvm-boot.sh')"
+	[[ "$output" == *mcastnet*extra-kvm-net.sh* ]]
+	$skip_step_if_already_done
+	runonvm "$DATADIR/$avmdir" <<<'sed -i "s/,addr=.*$//" kvm-boot.sh'  # because extra nics confict with preassigned pci slots
+	runonvm "$DATADIR/$avmdir" <<<'sed -i "s,mcastnet$,mcastnet \$(source extra-kvm-net.sh)," kvm-boot.sh'
+    ) ; prev_cmd_failed
+
+    (
+	avmdir=jhvmdir-hub
+	$starting_step "Add kvm nics to hub that listen on sockets"
+	runonvm "$DATADIR/$avmdir" <<<'[ -f extra-kvm-net.sh ]'
+	$skip_step_if_already_done
+	## a triple nested heredoc!  TODO: double check this, maybe rewrite
+	runonvm "$DATADIR/$avmdir" <<EOF
+cat >extra-kvm-net.sh <<'EOF2'
+cat <<EOFsourced
+$(
+    vvv=2
+    for i in "${vmlist[@]}"; do
+        [[ $i == *node* ]] || continue
+        newmac="52:54:00:12:50:$(( 10 + vvv ))"
+        echo
+        echo "-net nic,vlan=$vvv,macaddr=$newmac"
+        # ports 50 thru 79 are maybe free
+        echo "-net socket,vlan=$vvv,listen=:\$(( VNCPORT + 50 + $vvv ))"
+        (( vvv++ ))
+    done
+)
+EOFsourced
+EOF2
+EOF
+    ) ; prev_cmd_failed
 ) ; prev_cmd_failed
