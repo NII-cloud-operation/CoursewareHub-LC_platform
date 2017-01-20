941348402afb2a46a42af6cd8c82c9ece20b3b40
Richard Potter <potter@axsh.net>
2017-01-20 19:09:35 +0900

    Finished step to create aws VPC

--- ind-steps/aws-vpc-proxy/aws-vpc-proxy.sh
+++ ind-steps/aws-vpc-proxy/aws-vpc-proxy.sh
@@ -10,22 +10,32 @@ source "$(dirname $(readlink -f "$0"))/bashsteps-defaults-jan2017-check-and-do.s
 (
     $starting_step "Create VPC"
     [ "${vpckeypair=}" != '' ]  # the = is because of set -u
-    $skip_step_if_already_done ; set -e
+    $skip_step_if_already_done ; # (no set -e)
 
     vjson="$(aws ec2 describe-vpcs --filters "Name=tag:vpcname,Values=$VPCNAME" --query 'Vpcs[].VpcId')"
+    iferr_exit "describe-vpcs"
     # For example, vjson could be:
     # [
     # "vpc-fe7fdf9a"
     # ]
 
-    remove='[\[\]"]' # double quotes and brackets
+    remove='[,\[\]{}"]' # double quotes, commas, braces, and brackets
     ids=( ${vjson//$remove} )
     case "${#ids[@]}" in
 	1) # use the existing VPC
+	    echo "Using existing VPC: ${ids[0]}"
 	    echo "vpckeypair=\"${ids[0]}\"" >> "$DATADIR/datadir.conf"
 	;;
 	0) # create a new VPC
-	    reportfailed TODO
+	    awsout="$(aws ec2 create-vpc --cidr-block 192.168.11.0/24)"
+	    awsout="${awsout//$remove}"
+	    iferr_exit "create-vpc"
+	    read vpcid therest <<<"${awsout#*VpcId:}"  # parse line w/ "   VpcId: vpc-8f2d81eb "
+	    eval_iferr_exit '[[ "'$vpcid'" == vpc-* ]]'
+	    aws ec2 create-tags --resources "$vpcid" --tags "Key=vpcname,Value=$VPCNAME"
+	    iferr_exit "create-tags"
+	    echo "Created new VPC: $vpcid"
+	    echo "vpckeypair=\"$vpcid\"" >> "$DATADIR/datadir.conf"
 	    ;;
 	*)
 	    reportfailed "More than one VPC has tag with ($VPCNAME)"
