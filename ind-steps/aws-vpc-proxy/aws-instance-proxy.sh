a174a8e473cd18e20b783f46f379df8acd8f75ab
Richard Potter <potter@axsh.net>
2017-01-23 18:47:52 +0900

    Added step to associate global IP to VM.  (still needs a gateway on the vpc/subnet)

--- ind-steps/aws-vpc-proxy/aws-instance-proxy.sh
+++ ind-steps/aws-vpc-proxy/aws-instance-proxy.sh
@@ -28,3 +28,33 @@ eval_iferr_exit 'source "$DATADIR/vpc-datadir/datadir.conf"'
 ) ; $iferr_exit
 
 source "$DATADIR/datadir.conf"
+
+(
+    $starting_step "Associate new aws elastic IP to ${DATADIR##*/}"
+    [ "${associationid=}" != '' ]  # the = is because of set -u
+    $skip_step_if_already_done ; # (no set -e)
+
+    # step1: allocate
+    awsout="$(aws ec2 allocate-address --domain vpc)"
+    iferr_exit "run-instances"
+    remove='[,\[\]{}"]' # double quotes, commas, braces, and brackets
+    awsout="${awsout//$remove}"
+
+    read publicip therest <<<"${awsout#*PublicIp:}"  # parse line w/ "   PublicIp: 54.248.100.146 "
+    eval_iferr_exit '[ "${publicip//[^.]}" = "..." ]' # sanity checking for 3 dots
+    echo "publicip=\"$publicip\"" >> "$DATADIR/datadir.conf"
+
+    read allocationid therest <<<"${awsout#*AllocationId:}"  # parse line w/ "   AllocationId: eipalloc-d2b8acb7 "
+    echo "allocationid=\"$allocationid\"" >> "$DATADIR/datadir.conf"
+    eval_iferr_exit '[[ "'$allocationid'" == eipalloc-* ]]'
+
+    # step2: associate
+    awsout2="$(aws ec2 associate-address --instance-id "$instanceid" --allocation-id "$allocationid")"
+    iferr_exit "associate-address: $awsout2"
+
+    read associationid therest <<<"${awsout2#*AssociationId:}"  # parse line w/ "   AssociationId: eipassoc-2bebb745 "
+    echo "associationid=\"$associationid\"" >> "$DATADIR/datadir.conf"
+    eval_iferr_exit '[[ "'$associationid'" == eipassoc-* ]]'
+) ; $iferr_exit
+
+source "$DATADIR/datadir.conf"
