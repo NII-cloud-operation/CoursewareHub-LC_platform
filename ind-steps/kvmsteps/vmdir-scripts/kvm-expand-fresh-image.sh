05411ba9c8067c62c386615928df6210fa9b7f1e
Richard Potter <potter@axsh.net>
2017-01-31 09:25:10 +0900

    Converted kvm-expand-fresh-image.sh to use bashsteps

--- ind-steps/kvmsteps/vmdir-scripts/kvm-expand-fresh-image.sh
+++ ind-steps/kvmsteps/vmdir-scripts/kvm-expand-fresh-image.sh
@@ -16,21 +16,21 @@ fi
 source "$DATADIR/simple-defaults-for-bashsteps.source"
 source "$DATADIR/datadir.conf"
 
-if [ "$IMAGEFILENAME" != "" ];
-then
-    inuse="$(lsof "$DATADIR/$IMAGEFILENAME" 2>/dev/null)"
-    [ "$inuse" == "" ] || reportfailed "Existing image file in use ($IMAGEFILENAME)"
-    echo "Removing previous image file."
-    rm "$DATADIR/$IMAGEFILENAME" || reportfailed "Could not remove old image file"
-fi
+(
+    $starting_step "Expand VM image for ${DATADIR##*/}"
+    # the next line conveniently fails if $IMAGEFILENAME is null, but points
+    # to something awkward that needs some thought (TODO)
+    [ -f "$DATADIR/$IMAGEFILENAME" ]
+    $skip_step_if_already_done ; set -e
 
-echo -n "Expanding image file..."
-tar xzvf "$imagesource" -C "$DATADIR" >"$DATADIR"/tar.stdout || reportfailed "untaring of image"
-echo ".done."
-read IMAGEFILENAME rest <"$DATADIR"/tar.stdout
-[ "$rest" = "" ] || reportfailed "unexpected output from tar: $(<"$DATADIR"/tar.stdout)"
-echo 'IMAGEFILENAME="'$IMAGEFILENAME'"' >>"$DATADIR/datadir.conf"
-[ -f "${imagesource%.tar.gz}.sshuser" ] && cp "${imagesource%.tar.gz}.sshuser" "$DATADIR/sshuser"
-[ -f "${imagesource%.tar.gz}.sshkey" ] && {
-    cp "${imagesource%.tar.gz}.sshkey" "$DATADIR/sshkey"
-    chmod 600 "$DATADIR/sshkey" ; }
+    echo -n "Expanding image file..."
+    tar xzvf "$imagesource" -C "$DATADIR" >"$DATADIR"/tar.stdout || reportfailed "untaring of image"
+    echo ".done."
+    read IMAGEFILENAME rest <"$DATADIR"/tar.stdout
+    [ "$rest" = "" ] || reportfailed "unexpected output from tar: $(<"$DATADIR"/tar.stdout)"
+    echo 'IMAGEFILENAME="'$IMAGEFILENAME'"' >>"$DATADIR/datadir.conf"
+    [ -f "${imagesource%.tar.gz}.sshuser" ] && cp "${imagesource%.tar.gz}.sshuser" "$DATADIR/sshuser"
+    [ -f "${imagesource%.tar.gz}.sshkey" ] && {
+	cp "${imagesource%.tar.gz}.sshkey" "$DATADIR/sshkey"
+	chmod 600 "$DATADIR/sshkey" ; }
+)
