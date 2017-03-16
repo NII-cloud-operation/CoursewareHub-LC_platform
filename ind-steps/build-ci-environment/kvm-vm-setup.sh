5f75da69a9daed6ec56f4315e17d6f6f56cd25e8
t-iwano <t-iwano@axsh.net>
2017-03-16 19:32:46 +0900

    Removed unused code.

--- ind-steps/build-ci-environment/kvm-vm-setup.sh
+++ ind-steps/build-ci-environment/kvm-vm-setup.sh
@@ -2,21 +2,6 @@
 
 source "$(dirname $(readlink -f "$0"))/bashsteps-defaults-jan2017-check-and-do.source" || exit
 
-# These are expected to exist before running the first time:
-#conffiles=(
-#    datadir-jh.conf
-#    datadir-jh-hub.conf
-#    $(
-#	for i in $node_list; do
-#	    echo datadir-jh-$i.conf
-#	done
-#    )
-#)
-
-#for i in "${conffiles[@]}"; do
-#    [ -f "$DATADIR/$i" ] || reportfailed "$i is required"
-#done
-
 ## This script assumes link to ubuntu image is already at
 ## "$DATADIR/ubuntu-image-links/ubuntu-image.tar.gz"
 
@@ -73,81 +58,6 @@ EOF
 sudo apt-get update
 sudo apt-get -y install git
 EOF
-    ) ; $iferr_exit
-
-    (
-	$starting_group "Install ansible from source"
-	# Installing from source because of note here:
-	# https://github.com/compmodels/jupyterhub-deploy#deploying
-	# also because install with "apt-get -y install ansible" raised this
-	# problem: http://tracker.ceph.com/issues/12380
-
-	#  Source install instructions:
-	#  https://michaelheap.com/installing-ansible-from-source-on-ubuntu/
-	#  http://docs.ansible.com/ansible/intro_installation.html
-
-	[ -x "$DATADIR/$VMDIR/ssh-shortcut.sh" ] &&
-	    "$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF 2>/dev/null 1>/dev/null
-which ansible
-EOF
-	$skip_group_if_unnecessary
-
-	(
-	    $starting_step "Install ansible build dependencies"
-	    [ -x "$DATADIR/$VMDIR/ssh-shortcut.sh" ] &&
-		"$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF 2>/dev/null 1>/dev/null
-false # always do this, let group block it
-EOF
-	    $skip_step_if_already_done ; set -e
-
-	    "$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF
-sudo apt-get update
-sudo apt-get -y install python2.7 python-yaml python-paramiko python-jinja2 python-httplib2 make python-pip
-EOF
-	) ; $iferr_exit
-
-	(
-	    $starting_step "Clone ansible repository"
-	    [ -x "$DATADIR/$VMDIR/ssh-shortcut.sh" ] &&
-		"$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF 2>/dev/null 1>/dev/null
-[ -d ansible ]
-EOF
-	    $skip_step_if_already_done ; set -e
-
-	    "$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF
-set -e
-
-### git clone https://github.com/ansible/ansible.git --recursive
-
-git clone https://github.com/ansible/ansible.git
-cd ansible
-
-# reset to older version so that this error does not occur:
-#
-# TASK [nfs_server : bind home volume] *******************************************
-# fatal: [hub]: FAILED! => {"changed": false, "failed": true, "msg": "Error mounting //home: /bin/mount: invalid option -- 'T'
-
-git reset --hard a2d0bbed8c3f9de5d9c993e9b6f27f8af3eea438
-git submodule update --init --recursive
-
-EOF
-	) ; $iferr_exit
-
-	(
-	    $starting_step "Make/install ansible"
-	    [ -x "$DATADIR/$VMDIR/ssh-shortcut.sh" ] &&
-		"$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF 2>/dev/null 1>/dev/null
-[ -x /usr/local/bin/ansible ]
-EOF
-	    $skip_step_if_already_done ; set -e
-
-	    "$DATADIR/$VMDIR/ssh-shortcut.sh" <<EOF
-set -e
-cd ansible
-sudo make install
-EOF
-	) ; $iferr_exit
-
     ) ; $iferr_exit
 
 ) ; $iferr_exit
@@ -218,7 +128,6 @@ EOF
 	    hn=$(
 		case "$2" in
 		    *main*) echo main ;;
-		    *notebook*) echo notebook ;;
 		    *) reportfailed "BUG"
 		esac
 	      )
@@ -232,17 +141,6 @@ EOF
     }
 
     boot-one-vm "$VMDIR" "main KVM" datadir-ci.conf
-    boot-one-vm "$VMDIR-notebook" "notebook KVM" datadir-ci-notebook.conf
 
 ) ; $iferr_exit
 exit
-(
-    $starting_step "Make sure mac addresses were configured"
-    # Make sure all three mac addresses are unique
-    nodesarray=( $node_list )
-    vmcount=$(( ${#nodesarray[@]} + 2 )) # nodes + hub + ansible/main
-    [ $(grep -ho 'export.*mcastMAC.*' "$DATADIR"/jhvmdir*/*conf | sort -u | wc -l) -eq "$vmcount" ]
-    $skip_step_if_already_done
-    # always fail if this has not been done
-    reportfailed "Add mcastMAC= to: datadir-jh.conf datadir-jh-nodennn.conf"
-) ; $iferr_exit
