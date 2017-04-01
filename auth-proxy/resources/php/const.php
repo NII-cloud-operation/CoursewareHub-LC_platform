d0ba007ec3f95b43ddd217913af045a5b470955b
Kokichi Sasaki <k-sasaki@axsh.net>
2017-04-01 17:36:36 +0900

    add feature for none Gakunin federation.

--- auth-proxy/resources/php/const.php
+++ auth-proxy/resources/php/const.php
@@ -1,5 +1,5 @@
 <?php
-
+// Attribute dictionary for Gakunnin federation 
 const GF_ATTRIBUTES = array(
   "mail" => "urn:oid:0.9.2342.19200300.100.1.3",
   "eduPersonPrincipalName" => "urn:oid:1.3.6.1.4.1.5923.1.1.1.6",
@@ -8,4 +8,6 @@ const GF_ATTRIBUTES = array(
   "isMemberOf" => "urn:oid:1.3.6.1.4.1.5923.1.5.1.1",
 );
 
+// Path of IdP metadata file
+const IDP_METADATA_FILE_PATH = "/var/www/simplesamlphp/metadata/idp-proxy.xml";
 ?>
