edd04baf3f950738b2a72b938e1b906dccbd5d40
Kokichi Sasaki <k-sasaki@axsh.net>
2017-04-25 01:34:40 +0900

    Minor modifications

--- auth-proxy/resources/simplesamlphp/bin/get_idp_proxy_metadata.sh
+++ auth-proxy/resources/simplesamlphp/bin/get_idp_proxy_metadata.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-idp_proxy=$1
+idp_proxy="$1"
 
 cd /var/www/simplesamlphp/metadata
-curl --insecure -o idp_proxy.xml https://$idp_proxy/simplesaml/saml2/idp/metadata.php 
+curl --insecure -o idp_proxy.xml https://${idp_proxy}/simplesaml/saml2/idp/metadata.php 
