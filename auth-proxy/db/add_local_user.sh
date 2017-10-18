11b44921822a186daae23fb840071807ab874ab5
Richard Potter <potter@axsh.net>
2017-10-18 13:27:18 +0900

    Deleted whitespace

--- auth-proxy/db/add_local_user.sh
+++ auth-proxy/db/add_local_user.sh
@@ -7,9 +7,8 @@ mail_addr=$4
 
 "$hubdir"/jhvmdir-hub/ssh-shortcut.sh -q \
 sudo docker exec -i root_jpydb_1 \
-psql -U postgres -d jupyterhub << EOS  
+psql -U postgres -d jupyterhub << EOS
 
 INSERT INTO local_users VALUES(nextval(local_users_id_seq), $user_name, $hashed_password, $mail_addr); 
 
 EOS
-
--- auth-proxy/db/show_local_user.sh
+++ auth-proxy/db/show_local_user.sh
@@ -4,9 +4,8 @@ hubdir=$1
 
 "$hubdir"/jhvmdir-hub/ssh-shortcut.sh -q \
 sudo docker exec -i root_jpydb_1 \
-psql -U postgres -d jupyterhub << EOS  
+psql -U postgres -d jupyterhub << EOS
 
 SELECT * FROM local_users; 
 
 EOS
-
