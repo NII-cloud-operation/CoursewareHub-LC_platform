b103a260a346954ec36c59b0762f658980d24e6f
shingo78 <shingo.nakagawa@gmail.com>
2022-01-19 12:03:35 +0900

    Use jinja2 templates for generating configurations at startup

--- auth-proxy/resources/scripts/start.sh
+++ auth-proxy/resources/scripts/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
 
-/config.py
+j2 /etc/templates/nginx.conf.j2 -o /etc/nginx/nginx.conf
+j2 /etc/templates/module_cron.php.j2 -o /var/www/simplesamlphp/config/module_cron.php
+j2 /etc/templates/cron_root.j2 -o /var/spool/cron/crontabs/root
+
+chmod 600 /var/spool/cron/crontabs/root
+
+if [ -n "${AUTH_FQDN}" ] ; then
+    touch /var/www/simplesamlphp/modules/metarefresh/enable
+    j2 /etc/templates/config-metarefresh.php.j2 -o /var/www/simplesamlphp/config/config-metarefresh.php
+    j2 /etc/templates/authsources.php.j2 -o /var/www/simplesamlphp/config/authsources.php
+else
+    rm -f /var/www/simplesamlphp/modules/metarefresh/enable
+fi
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf
