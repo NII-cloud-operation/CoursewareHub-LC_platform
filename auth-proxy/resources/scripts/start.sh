5d20d48a3197a996f10fff0f625af1ae905222fa
shingo78 <shingo.nakagawa@gmail.com>
2024-02-21 17:55:59 +0900

    Upgrade SimleSAMLphp to 2.1

--- auth-proxy/resources/scripts/start.sh
+++ auth-proxy/resources/scripts/start.sh
@@ -3,6 +3,8 @@ set -e
 
 TEMPLATE_DIR=/etc/templates
 
+j2 ${TEMPLATE_DIR}/embedded-wayf-config.js.j2 -o /var/www/simplesamlphp/templates/includes/embedded-wayf-config.js
+j2 ${TEMPLATE_DIR}/embedded-wayf-loader.js.j2 -o /var/www/simplesamlphp/templates/includes/embedded-wayf-loader.js
 j2 ${TEMPLATE_DIR}/nginx.conf.j2 -o /etc/nginx/nginx.conf
 j2 ${TEMPLATE_DIR}/config.php.j2 -o /var/www/simplesamlphp/config/config.php
 j2 ${TEMPLATE_DIR}/module_cron.php.j2 -o /var/www/simplesamlphp/config/module_cron.php
@@ -12,22 +14,16 @@ chmod 600 /var/spool/cron/crontabs/root
 
 if [[ -n "${AUTH_FQDN}" ]] ; then
     # Join federation via IdP Proxy
-    touch /var/www/simplesamlphp/modules/metarefresh/enable
-    j2 ${TEMPLATE_DIR}/config-metarefresh.php.j2 -o /var/www/simplesamlphp/config/config-metarefresh.php
+    j2 ${TEMPLATE_DIR}/module_metarefresh.php.j2 -o /var/www/simplesamlphp/config/module_metarefresh.php
     j2 ${TEMPLATE_DIR}/authsources.php.j2 -o /var/www/simplesamlphp/config/authsources.php
 elif [[ "$ENABLE_FEDERATION" == "1" || "$ENABLE_FEDERATION" == "yes" ]]; then
     # Join federation directly
-    touch /var/www/simplesamlphp/modules/metarefresh/enable
     if [[ "$ENABLE_TEST_FEDERATION" == "1" || "$ENABLE_TEST_FEDERATION" == "yes" ]]; then
-        j2 ${TEMPLATE_DIR}/federation/config-metarefresh-test.php.j2 -o /var/www/simplesamlphp/config/config-metarefresh.php
+        j2 ${TEMPLATE_DIR}/federation/module_metarefresh-test.php.j2 -o /var/www/simplesamlphp/config/module_metarefresh.php
     else
-        j2 ${TEMPLATE_DIR}/federation/config-metarefresh.php.j2 -o /var/www/simplesamlphp/config/config-metarefresh.php
+        j2 ${TEMPLATE_DIR}/federation/module_metarefresh.php.j2 -o /var/www/simplesamlphp/config/module_metarefresh.php
     fi
-    j2 ${TEMPLATE_DIR}/federation/selectidp-dropdown.php.j2 -o /var/www/simplesamlphp/templates/selectidp-dropdown.php
     j2 ${TEMPLATE_DIR}/authsources.php.j2 -o /var/www/simplesamlphp/config/authsources.php
-else
-    # without federation
-    rm -f /var/www/simplesamlphp/modules/metarefresh/enable
 fi
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
